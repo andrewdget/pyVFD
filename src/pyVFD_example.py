434f216579dd8a324eb11aea989441d0065b1691
Andrew Getman <andrewdget@gmail.com>
2023-12-21 15:26:48 -0700

    improved example program

--- src/pyVFD_example.py
+++ src/pyVFD_example.py
@@ -14,52 +14,28 @@ import time
 
 def example_routine():
 	current = datetime.datetime.now()
-	hours = str(current.hour)
-	minutes = str(current.minute)
-	seconds = str(current.second)
-	microseconds = str(round(current.microsecond*10**-5))
+	hr = str(format(current.hour, '02d'))
+	mn = str(format(current.minute, '02d'))
+	se = str(format(current.second, '02d'))
+	mi = str(round(current.microsecond*10**-5))
 
-	if len(hours) == 1:
-		hr1 = '0'
-		hr2 = hours
-	else:
-		hr1 = hours[0]
-		hr2 = hours[1]
+	# clear display so it can be changed
+	d_hr1.clear()
+	d_hr2.clear()
+	d_min1.clear()
+	d_min2.clear()
+	d_sec1.clear()
+	d_sec2.clear()
+	d_mic.clear()
 
-	if len(minutes) == 1:
-		min1 = '0'
-		min2 = minutes
-	else:
-		min1 = minutes[0]
-		min2 = minutes[1]
-
-	if len(seconds) == 1:
-		sec1 = '0'
-		sec2 = seconds
-	else:
-		sec1 = seconds[0]
-		sec2 = seconds[1]
-
-	if microseconds == '10':
-		micro = '0'
-	else:
-		micro = microseconds
-
-	disp_hr1.clear()
-	disp_hr2.clear()
-	disp_min1.clear()
-	disp_min2.clear()
-	disp_sec1.clear()
-	disp_sec2.clear()
-	disp_micro.clear()
-
-	disp_hr1.char(hr1).pack(side=tk.LEFT)
-	disp_hr2.char(hr2, CC=1).pack(side=tk.LEFT)
-	disp_min1.char(min1).pack(side=tk.LEFT)
-	disp_min2.char(min2, CC=1).pack(side=tk.LEFT)
-	disp_sec1.char(sec1).pack(side=tk.LEFT)
-	disp_sec2.char(sec2, DP=1).pack(side=tk.LEFT)
-	disp_micro.char(micro).pack(side=tk.LEFT)
+	# update and pack display into root
+	d_hr1.char(hr[0]).pack(side=tk.LEFT)
+	d_hr2.char(hr[1], CC=1).pack(side=tk.LEFT)
+	d_min1.char(mn[0]).pack(side=tk.LEFT)
+	d_min2.char(mn[1], CC=1).pack(side=tk.LEFT)
+	d_sec1.char(se[0]).pack(side=tk.LEFT)
+	d_sec2.char(se[1], DP=1).pack(side=tk.LEFT)
+	d_mic.char(mi[0]).pack(side=tk.LEFT)
 
 	root.after(100, example_routine)
 
@@ -68,25 +44,29 @@ def example_routine():
 root = tk.Tk()
 root.title('pyVFD Example')
 
-global_height=200
+gh=200
 
-disp_hr1 = pyVFD.seg7(root, height=global_height)
-disp_hr2 = pyVFD.seg7(root, height=global_height, use_CC=True)
-disp_min1 = pyVFD.seg7(root, height=global_height)
-disp_min2 = pyVFD.seg7(root, height=global_height, use_CC=True)
-disp_sec1 = pyVFD.seg7(root, height=global_height)
-disp_sec2 = pyVFD.seg7(root, height=global_height, use_DP=True)
-disp_micro = pyVFD.seg7(root, height=global_height)
+# build displays
+d_hr1 = pyVFD.seg7(root, height=gh, off_color=[51,0,0], on_color=[255,0,0])
+d_hr2 = pyVFD.seg7(root, height=gh, off_color=[51,25,0], on_color=[255,128,0],
+	 use_CC=True)
+d_min1 = pyVFD.seg7(root, height=gh, off_color=[51,51,0], on_color=[255,255,0])
+d_min2 = pyVFD.seg7(root, height=gh, off_color=[25,51,0], on_color=[128,255,0],
+	use_CC=True)
+d_sec1 = pyVFD.seg7(root, height=gh, off_color=[0,51,51], on_color=[0,255,255])
+d_sec2 = pyVFD.seg7(root, height=gh, off_color=[25,0,51], on_color=[127,0,255],
+	use_DP=True)
+d_mic = pyVFD.seg7(root, height=gh, off_color=[32,32,32], on_color=[255,255,255])
 
-disp_hr1.char('off').pack(side=tk.LEFT)
-disp_hr2.char('off').pack(side=tk.LEFT)
-disp_min1.char('off').pack(side=tk.LEFT)
-disp_min2.char('off').pack(side=tk.LEFT)
-disp_sec1.char('off').pack(side=tk.LEFT)
-disp_sec2.char('off').pack(side=tk.LEFT)
-disp_micro.char('off').pack(side=tk.LEFT)
+# set initial condition
+d_hr1.char('off').pack(side=tk.LEFT)
+d_hr2.char('off').pack(side=tk.LEFT)
+d_min1.char('off').pack(side=tk.LEFT)
+d_min2.char('off').pack(side=tk.LEFT)
+d_sec1.char('off').pack(side=tk.LEFT)
+d_sec2.char('off').pack(side=tk.LEFT)
+d_mic.char('off').pack(side=tk.LEFT)
 
 example_routine()
 
 root.mainloop()
-
